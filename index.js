47e0f41e3d65596d34a6a4ab6df6f16c99d09826
Declan Rek <declanrek@gmail.com>
2019-05-02 23:54:38 +0200

    solution 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import './index.css'
-import initLanguageSelect from './modules/languageSelect'
-import initCaseSelect from './modules/caseSelect'
+import './index.css';
+import initLanguageSelect from './modules/languageSelect';
+import initCaseSelect from './modules/caseSelect';
 
 initLanguageSelect();
 initCaseSelect();
